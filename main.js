a1eda9de8b70bdc4582497cefeec7dfb7c85f9e4
helenhryshel <helenhryshel@gmail.com>
2023-01-26 14:10:29 +0300

    Add arrow functions

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-function sum(a) {
-  return function (b) {
+const sum = a => {
+  return b => {
     return a + b;
   };
-}
-
+};
 console.log(sum(2)(4));
-
-
 
 const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
@@ -14,19 +11,17 @@ const text3 = document.getElementById('text3');
 
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
-function getColor() {
+const getColor = () => {
   let i = 0;
-  return function () {
-    this.style.color = colors[i];
+  return () => {
+    event.target.style.color = colors[i];
     i++;
     if (i > 4) {
       i = 0;
     }
   };
-}
+};
 
 text1.addEventListener('click', getColor());
-
 text2.addEventListener('click', getColor());
-
 text3.addEventListener('click', getColor());
