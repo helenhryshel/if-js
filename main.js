1b3659a4329d50cd2a59aee24d5f4d1cd779c5d9
helenhryshel <helenhryshel@gmail.com>
2023-02-23 13:24:19 +0300

    Add changeStyle function

--- main.js
+++ main.js
@@ -1,95 +1,23 @@
-class User {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-const User1 = new User('Ann', 'Hint');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-console.log(User1.fullName);
+const colors = {
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  index: 0,
 
-console.log('---------------');
-
-class Student extends User {
-  constructor(
-    firstName,
-    lastName,
-    admissionYear,
-    courseName,
-    currentYear = 2023)
-  {
-    super(firstName, lastName);
-    this.admissionYear = admissionYear;
-    this.currentYear = currentYear;
-    this.courseName = courseName;
-  }
-  get course() {
-    return this.currentYear - this.admissionYear;
-  }
-}
-
-const student1 = new Student('Василий', 'Петров', 2019, 'Java');
-
-console.log(student1);
-
-console.log('---------------');
-
-const studentsData = [
-  {
-    firstName: 'Василий',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Java',
+  next() {
+    if (colors.index === colors.data.length) {
+      colors.index = 0;
+    }
+    return { done: false, value: colors.data[colors.index++] };
   },
-  {
-    firstName: 'Иван',
-    lastName: 'Иванов',
-    admissionYear: 2018,
-    courseName: 'JavaScript',
-  },
-  {
-    firstName: 'Александр',
-    lastName: 'Федоров',
-    admissionYear: 2017,
-    courseName: 'Python',
-  },
-  {
-    firstName: 'Николай',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Android',
-  },
-];
+};
 
-class Students {
-  constructor(array) {
-    this.array = array;
-  }
-  getInfo() {
-    const sortStList = [];
-    this.array.forEach((element) => {
-      const student = new Student(
-        element.firstName,
-        element.lastName,
-        element.admissionYear,
-        element.courseName,
-      );
-      sortStList.push(student);
-    });
-    sortStList.sort(function (a, b) {
-      return a['course'] - b['course'];
-    });
-    const result = [];
-    sortStList.forEach((element) => {
-      result.push(`${element.fullName} - ${element.courseName}, ${element.course} курс`);
-    });
+const changeStyle = id => event => {
+  event.target.style.color = colors.next(id).value;
+};
 
-    return result;
-  }
-}
-
-const students = new Students(studentsData);
-console.log(students.getInfo());
+text1.addEventListener('click', changeStyle('text1'));
+text2.addEventListener('click', changeStyle('text2'));
+text3.addEventListener('click', changeStyle('text3'));
