8f980bdefa9bb0fa621deb638ae484fdd1edfdd6
helenhryshel <helenhryshel@gmail.com>
2023-03-19 23:25:30 +0300

    add changes to changeStyle

--- main.js
+++ main.js
@@ -2,22 +2,25 @@ const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
 const text3 = document.getElementById('text3');
 
-const colors = {
-  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
-  index: 0,
+const changeStyle = () => {
+  let index = 0;
 
-  next() {
-    if (colors.index === colors.data.length) {
-      colors.index = 0;
-    }
-    return { done: false, value: colors.data[colors.index++] };
-  },
+  const colors = {
+    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+
+    next() {
+      if (index === colors.data.length) {
+        index = 0;
+      }
+      return { done: false, value: colors.data[index++] };
+    },
+  };
+
+  return (event) => {
+    event.target.style.color = colors.next().value;
+  };
 };
 
-const changeStyle = (id) => (event) => {
-  event.target.style.color = colors.next(id).value;
-};
-
-text1.addEventListener('click', changeStyle('text1'));
-text2.addEventListener('click', changeStyle('text2'));
-text3.addEventListener('click', changeStyle('text3'));
+text1.addEventListener('click', changeStyle());
+text2.addEventListener('click', changeStyle());
+text3.addEventListener('click', changeStyle());
