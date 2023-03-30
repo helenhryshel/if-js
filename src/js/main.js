80353633a63ce565e4acd2ad85eac1f12e358549
helenhryshel <helenhryshel@gmail.com>
2023-03-30 13:20:00 +0300

    lesson-11: fix remarks

--- src/js/main.js
+++ src/js/main.js
@@ -64,126 +64,179 @@ const data = [
       'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
   },
 ];
+const guestItem = document.createElement('div');
+guestItem.className = 'guests-container__item';
 
-const pictureSlides = (id, data) => {
-  for (const element in data) {
-    const guestItem = document.createElement('div');
-    guestItem.className = 'guests-container__item';
-    guestItem.id = data[element].id;
-    const guestsImg = document.createElement('div');
-    const image = document.createElement('img');
-    image.src = data[element].imageUrl;
-    const places = document.createElement('div');
-    const guestHotel = document.createElement('p');
-    guestHotel.className = 'guests-container__name';
-    const textNameHotel = document.createTextNode(data[element].name);
-    const guestPlace = document.createElement('p');
-    guestPlace.className = 'guests-container__place';
-    const textNameCountry = document.createTextNode(
-      `${data[element].city}, ${data[element].country}`,
+const hotelsMarkup = data
+  .map(
+    (hotel) =>
+      `<div>
+    <img src="${hotel.imageUrl}" id="${hotel.id}">
+    <div>
+    <p class="guests-container__name">${hotel.name}</p>
+    <p class="guests-container__place">${hotel.city}, ${hotel.country}</p>
+    </div>
+    </div>`,
+  )
+  .join('');
+
+guestItem.innerHTML = hotelsMarkup;
+document.getElementById('guests-container__navigation').appendChild(guestItem);
+
+const optionsData = {
+  adults: {
+    min: 1,
+    max: 30,
+    value: 1,
+  },
+  children: {
+    min: 0,
+    max: 10,
+    value: 0,
+  },
+  rooms: {
+    min: 1,
+    max: 30,
+    value: 1,
+  },
+};
+
+const searchInfo = document.getElementById('sectionPeople');
+const optionsText = document.getElementById('myDropdown');
+optionsText.placeholder = `${optionsData.adults.value} Adults — ${optionsData.children.value} Children — ${optionsData.rooms.value} Room`;
+
+const createOptionsDiv = () => {
+  const optionsDiv = document.createElement('div');
+  optionsDiv.classList.add('options');
+  optionsDiv.setAttribute('id', 'options');
+  optionsDiv.innerHTML = `<div class="options-items" id="options-items"></div>
+      <div class="options-text-div" id="options-text-div"></div>
+      <div class="options-select-items" id="options-select-items"></div>`;
+  searchInfo.appendChild(optionsDiv);
+  createOptions();
+  searchInfo.removeEventListener('click', createOptionsDiv);
+};
+
+const createOptions = () => {
+  const optionsItems = document.getElementById('options-items');
+  Object.keys(optionsData).forEach((option) => {
+    const optionsItem = document.createElement('div');
+    optionsItem.classList.add('options-item');
+    optionsItem.innerHTML = `
+        <div class="options-description-text">
+          <span>${capitalizeFirstLetter(option)}</span>
+        </div>
+        <div class="options-item-buttons">
+          <button class="options-button options-minus-button_js" id="options-minus-button-${option}"  type="button" >—</button>
+          <span class="options-counter-number" id="options-counter-number-${option}">${
+      optionsData[option].value
+    }</span>
+          <button class="options-button options-plus-button_js" id="options-plus-button-${option}" type="button">+</button>
+        </div>
+      `;
+    optionsItems.appendChild(optionsItem);
+    document
+      .getElementById(`options-plus-button-${option}`)
+      .addEventListener('click', () => addOne(option));
+    document
+      .getElementById(`options-minus-button-${option}`)
+      .addEventListener('click', () => removeOne(option));
+  });
+};
+
+const capitalizeFirstLetter = (option) =>
+  option[0].toUpperCase() + option.slice(1);
+
+const addChildrenAge = () => {
+  const ageChildrenBlock = document.getElementById('options-select-items');
+  const ageOptions = [...Array(18)]
+    .map(
+      (_, index) =>
+        `<option value=${index}>${index} ${
+          index === 1 ? 'year old' : 'years old'
+        }</option>`,
+    )
+    .join('');
+
+  ageChildrenBlock.innerHTML += `<div class="options-select-item"><select id="options-child-age" class="options-child-age-select" name="options-child-age">${ageOptions}</select></div>`;
+};
+
+const removeChildrenAge = () => {
+  const ageChildrenBlock = document.getElementById('options-select-items');
+  const lastSelectItem = document.querySelector(
+    '.options-select-item:last-child',
+  );
+  ageChildrenBlock.removeChild(lastSelectItem);
+};
+
+const addChildrenAgeQuestion = () => {
+  const optionsTextDiv = document.getElementById('options-text-div');
+  optionsTextDiv.innerHTML = `
+    <span class="options-text" id="options-text">What is the age of the child you’re travelling with?</span>
+  `;
+};
+
+const removeChildrenAgeQuestion = () => {
+  const optionsTextDiv = document.getElementById('options-text-div');
+  optionsTextDiv.innerHTML = '';
+};
+
+const addOne = (optionName) => {
+  if (optionsData[optionName].value < optionsData[optionName].max) {
+    optionsData[optionName].value++;
+    if (optionName === 'children') {
+      addChildrenAge();
+    }
+  }
+  if (optionsData[optionName].value === optionsData[optionName].max) {
+    const plusButton = document.getElementById(
+      `options-plus-button-${optionName}`,
     );
-    guestsImg.appendChild(image);
-    guestItem.appendChild(guestsImg);
-    guestsImg.appendChild(places);
-    places.appendChild(guestHotel);
-    places.appendChild(guestPlace);
-    guestHotel.appendChild(textNameHotel);
-    guestPlace.appendChild(textNameCountry);
-
-    document.getElementById(id).appendChild(guestItem);
+    plusButton.setAttribute('disabled', 'disabled');
   }
+  if (optionsData[optionName].value > optionsData[optionName].min) {
+    const minusButton = document.getElementById(
+      `options-minus-button-${optionName}`,
+    );
+    minusButton.removeAttribute('disabled');
+  }
+  if (optionName === 'children' && optionsData[optionName].value === 1) {
+    addChildrenAgeQuestion();
+  }
+  refreshOptionCounter(optionName);
 };
 
-pictureSlides('guests-container__navigation', data);
-
-const myDropdown = document.getElementById('myDropdown');
-myDropdown.onclick = () => {
-  document.getElementById('dropdown').classList.toggle('show');
-};
-
-window.onclick = function (event) {
-  if (!event.target.matches('.top-section__people')) {
-    const dropdowns = document.getElementsByClassName('dropdown');
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+const removeOne = (optionName) => {
+  if (optionsData[optionName].value > optionsData[optionName].min) {
+    optionsData[optionName].value--;
+    if (optionName === 'children') {
+      removeChildrenAge();
     }
   }
-};
-
-const minusAdults = document.getElementById('minusAdults');
-const plusAdults = document.getElementById('plusAdults');
-const countNumber = document.getElementById('countNumber');
-const minusChild = document.getElementById('minusChild');
-const plusChild = document.getElementById('plusChild');
-const countNumber2 = document.getElementById('countNumber2');
-const minusRooms = document.getElementById('minusRooms');
-const plusRooms = document.getElementById('plusRooms');
-const countNumber3 = document.getElementById('countNumber3');
-
-const counts = { Adults: 1, Child: 0, Rooms: 1 };
-const countsElement = {
-  Adults: countNumber,
-  Child: countNumber2,
-  Rooms: countNumber3,
-};
-
-const countCalc = (limits, sign, reserveEl) => {
-  if (sign === 'plus') {
-    if (counts[reserveEl] < limits) {
-      counts[reserveEl]++;
-      if (reserveEl === 'Child') {
-        childSection(counts['Child'], sign);
-      }
-    }
-  } else {
-    if (counts[reserveEl] >= limits) {
-      counts[reserveEl]--;
-      if (reserveEl === 'Child') {
-        childSection(counts['Child'], sign);
-      }
-    }
+  if (optionsData[optionName].value === optionsData[optionName].min) {
+    const minusButton = document.getElementById(
+      `options-minus-button-${optionName}`,
+    );
+    minusButton.setAttribute('disabled', 'disabled');
   }
-  countsElement[reserveEl].innerHTML = counts[reserveEl];
-  myDropdown.placeholder = `${counts['Adults']} Adults — ${counts['Child']} Children — ${counts['Rooms']} Room`;
+  if (optionsData[optionName].value < optionsData[optionName].max) {
+    const plusButton = document.getElementById(
+      `options-plus-button-${optionName}`,
+    );
+    plusButton.removeAttribute('disabled');
+  }
+  if (optionName === 'children' && optionsData[optionName].value < 1) {
+    removeChildrenAgeQuestion();
+  }
+  refreshOptionCounter(optionName);
 };
 
-plusAdults.addEventListener('click', () => countCalc(30, 'plus', 'Adults'));
-minusAdults.addEventListener('click', () => countCalc(2, 'minus', 'Adults'));
-plusChild.addEventListener('click', () => countCalc(10, 'plus', 'Child'));
-minusChild.addEventListener('click', () => countCalc(1, 'minus', 'Child'));
-plusRooms.addEventListener('click', () => countCalc(30, 'plus', 'Rooms'));
-minusRooms.addEventListener('click', () => countCalc(2, 'minus', 'Rooms'));
-
-myDropdown.placeholder = `${counts['Adults']} Adults — ${counts['Child']} Children — ${counts['Rooms']} Room`;
-
-const childText = document.createElement('p');
-childText.hidden = true;
-childText.appendChild(
-  document.createTextNode(
-    'What is the age of the child you’re travelling with?',
-  ),
-);
-document.getElementById('counterChild').appendChild(childText);
-
-const childSection = (num, sign) => {
-  childText.hidden = num <= 0;
-  const selectAge = document.createElement('select');
-  selectAge.id = 'selectAge' + num;
-  for (let i = 0; i < 18; i++) {
-    const childAge = document.createElement('option');
-    selectAge.className = 'age';
-    childAge.value = i;
-    childAge.text = `${i} years old`;
-    selectAge.appendChild(childAge);
-  }
-  if (sign === 'plus') {
-    document.getElementById('counterChild').appendChild(selectAge);
-  } else {
-    num++;
-    document.getElementById('selectAge' + num).remove();
-  }
+const refreshOptionCounter = (optionName) => {
+  const optionNumber = document.getElementById(
+    `options-counter-number-${optionName}`,
+  );
+  optionNumber.textContent = optionsData[optionName].value;
+  optionsText.placeholder = `${optionsData.adults.value} Adults — ${optionsData.children.value} Children — ${optionsData.rooms.value} Room`;
 };
+
+searchInfo.addEventListener('click', createOptionsDiv);
